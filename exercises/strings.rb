076e31d2cdc28e9c6a8c4d79c129514337b7fe58
Jair Jaramillo <jair.jaramillo@outlook.com>
2019-11-25 10:09:08 -0600

    rubocop fix

--- exercises/strings.rb
+++ exercises/strings.rb
@@ -1,23 +1,36 @@
-#!/bin/ruby
+# frozen_string_literal: true
 
 require 'json'
 require 'stringio'
 
 # Complete the repeatedString function below.
-def repeatedString(s, n)
+def repeatedString(s, n) # rubocop:disable Naming/MethodName, Naming/UncommunicativeMethodParamName
+  result = s
+  counter = 0
 
+  (n - s.size).times do
+    counter = 0 if counter >= s.size
+    result += s[counter]
+    counter += 1
+  end
 
+  result.count('a')
 end
 
-fptr = File.open(ENV['OUTPUT_PATH'], 'w')
+# fptr = File.open(ENV['OUTPUT_PATH'], 'w')
 
-s = gets.to_s.rstrip
+# s = gets.to_s.rstrip
 
-n = gets.to_i
+# n = gets.to_i
+
+s = 'aba'
+n = 10
 
 result = repeatedString s, n
 
-fptr.write result
-fptr.write "\n"
+puts result
 
-fptr.close()
+# fptr.write result
+# fptr.write "\n"
+
+# fptr.close
--- replit/replit_1.7.rb
+++ replit/replit_1.7.rb
@@ -7,20 +7,22 @@
 # Creates a Stack linked list
 def balanced_brackets?(string)
   stack = []
-  string = string.tr("a-zA-Z", '')
+  string = string.tr('a-zA-Z', '')
   string = string.split('')
   string.size.times do |x|
     if string[x] == '(' || string[x] == '[' || string[x] == '{'
       stack.push(string[x])
     else
       return false if stack.empty?
+
       top = stack.pop
-      return false if (top == '(' && string[x] != ')')
-      return false if (top == '{' && string[x] != '}')
-      return false if (top == '[' && string[x] != ']')
+      return false if top == '(' && string[x] != ')'
+      return false if top == '{' && string[x] != '}'
+      return false if top == '[' && string[x] != ']'
     end
   end
   return true if stack.empty?
+
   false
 end
 
